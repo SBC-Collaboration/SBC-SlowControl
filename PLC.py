29cc7610b6fbddb0efcd13f6a72fcf39de9f997b
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2021-10-04 15:56:10 -0700

    Update PLC.py

--- PLC.py
+++ PLC.py
@@ -310,10 +310,10 @@ class PLC:
         print("write open result=", Raw)
 
     def ReadCoil(self):
-        Raw_BO = self.Client_BO.read_holding_registers(12296, count=1, unit=0x01)
+        Raw_BO = self.Client_BO.read_coils(12296, count=1, unit=0x01)
         print("Raw coil",Raw_BO)
-        output_BO = struct.pack("H", Raw_BO.getRegister(0))
-        print("valve value is", output_BO)
+        output_BO = struct.pack("H", Raw_BO.bits[0])
+        print("Coil: valve value is", output_BO)
 
     def WriteClose(self):
         # Raw = self.Client_BO.write_register(12289, value=b'\x00\x00\x00\x04', unit=0x01)
