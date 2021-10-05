a450cb0fde2272b63ad9ecacfd441624761e4e47
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2021-10-04 17:16:11 -0700

    Update PLC.py

--- PLC.py
+++ PLC.py
@@ -152,7 +152,7 @@ class PLC:
                 try:
                     rr =self.Client_BO.read_coils(i,count=1,unit=0x01)
                     print(i,"succeed")
-                    print(rr.bits[0])
+                    assert(rr.bits[0]==True)
                 except:
                     print("error")
                     pass
