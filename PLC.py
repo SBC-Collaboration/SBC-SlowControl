a43ed2f9ea0bfa8be5e488049f7ab4a2b0d82ade
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2021-10-15 14:59:56 -0700

    Update PLC.py

--- PLC.py
+++ PLC.py
@@ -318,7 +318,7 @@ class PLC:
             for key in self.TT_BO_address:
                 Raw_BO_TT_BO[key] = self.Client_BO.read_holding_registers(self.TT_BO_address[key], count=2, unit=0x01)
                 self.TT_BO_dic[key] = round(
-                    struct.unpack(">f", struct.pack("<HH", Raw_BO_TT_BO[key].getRegister(0 + 1), Raw_BO_TT_BO[key].getRegister(0)))[0], 3)
+                    struct.unpack(">f", struct.pack(">HH", Raw_BO_TT_BO[key].getRegister(0 + 1), Raw_BO_TT_BO[key].getRegister(0)))[0], 3)
                 print(key, "little endian", hex(Raw_BO_TT_BO[key].getRegister(1)),"big endian",hex(Raw_BO_TT_BO[key].getRegister(0)))
                 print(key, "'s' value is", self.TT_BO_dic[key])
 
