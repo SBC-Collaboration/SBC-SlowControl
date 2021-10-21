ec6b9044ba7e69e9e61ae0794ca89e9451853acd
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2021-10-21 16:51:32 -0700

    Update SlowDAQ_SBC_v2.py

--- SlowDAQ_SBC_v2.py
+++ SlowDAQ_SBC_v2.py
@@ -917,13 +917,13 @@ class MainWindow(QtWidgets.QMainWindow):
             lambda : self.BOTTBoxUpdate(pid=self.AlarmButton.SubWindow.TT2119.Label.text(),Act=self.AlarmButton.SubWindow.TT2119.AlarmMode.isChecked(),
                                          LowLimit=self.AlarmButton.SubWindow.TT2119.Low_Limit.Field.text(), HighLimit=self.AlarmButton.SubWindow.TT2119.High_Limit.Field.text()))
 
-        self.AlarmButton.SubWindow.TT2119.Low_Limit.Field.editingFinished.connect(
+        self.AlarmButton.SubWindow.TT2119.Low_Limit.Field.returnPressed.connect(
             lambda : self.BOTTBoxUpdate(pid=self.AlarmButton.SubWindow.TT2119.Label.text(),
                                          Act=self.AlarmButton.SubWindow.TT2119.AlarmMode.isChecked(),
                                          LowLimit=self.AlarmButton.SubWindow.TT2119.Low_Limit.Field.text(),
                                          HighLimit=self.AlarmButton.SubWindow.TT2119.High_Limit.Field.text()))
 
-        self.AlarmButton.SubWindow.TT2119.High_Limit.Field.editingFinished.connect(
+        self.AlarmButton.SubWindow.TT2119.High_Limit.Field.returnPressed.connect(
             lambda: self.BOTTBoxUpdate(pid=self.AlarmButton.SubWindow.TT2119.Label.text(),
                                        Act=self.AlarmButton.SubWindow.TT2119.AlarmMode.isChecked(),
                                        LowLimit=self.AlarmButton.SubWindow.TT2119.Low_Limit.Field.text(),
