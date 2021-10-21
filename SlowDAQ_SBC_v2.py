388b147c7f3405aa8bf27025442075a6e3edb872
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2021-10-21 10:54:48 -0700

    Update SlowDAQ_SBC_v2.py

--- SlowDAQ_SBC_v2.py
+++ SlowDAQ_SBC_v2.py
@@ -4029,6 +4029,7 @@ class UpdateDisplay(QtCore.QObject):
                 # print(type(dic))
 
                 self.MW.AlarmButton.SubWindow.TT2119.UpdateAlarm(self.Client.receive_dic["Alarm"]["TT"]["BO"]["TT2119"])
+                print("alarmbutton",self.Client.receive_dic["Alarm"]["TT"]["BO"]["TT2119"])
                 self.MW.AlarmButton.SubWindow.TT2119.Indicator.SetValue(self.Client.receive_dic["data"]["TT"]["BO"]["TT2119"])
 
                 self.display_update.emit(dic)
