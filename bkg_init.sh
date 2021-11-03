b855e553465c01ad37e6d94b61d1b8856e458d43
RunzZhang <runzezhang@ucsb.edu>
2021-11-03 16:04:59 -0700

    bkg code

--- bkg_init.sh
+++ bkg_init.sh
@@ -5,6 +5,6 @@
 PATH=/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/home/hep/.local/bin:/home/hep/bin
 
 #xterm -hold -e "conda init bash;source ~/conda_init.sh ; conda activate sbcslowcontrol; source PLC_init.sh & disown"
-xterm -hold -e  "nohup sh PLC_init.sh > /dev/null 2>&1"
+xterm -hold -e  "nohup sh PLC_init.sh > /dev/null 2>&1 &"
 
 
