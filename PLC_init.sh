e4725741740818a47681d5a88723c272ee1a70b7
RunzZhang <runzezhang@ucsb.edu>
2021-11-03 15:56:47 -0700

    bkg code

--- PLC_init.sh
+++ PLC_init.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 PATH=/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/home/hep/.local/bin:/home/hep/bin
+source ~/conda_init.sh 
+source /home/hep/anaconda3/bin/activate sbcslowcontrol 
+which python
 while true; do
-source ~/Downloads/sbc_slowcontrol/SBC_slowcontrol/clear_tcp.sh
-python ~/Downloads/sbc_slowcontrol/SBC_slowcontrol/PLC.py
+source /home/hep/Downloads/sbc_slowcontrol/SBC-SlowControl/clear_tcp.sh
+python /home/hep/Downloads/sbc_slowcontrol/SBC-SlowControl/PLC.py
 sleep 2
 done
 echo "quit the loop"
