cdcaf9fbef8f948e5ee71fadf4ab43da7c7271d3
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2021-11-05 16:32:53 -0700

    Update PLC.py

--- PLC.py
+++ PLC.py
@@ -314,6 +314,7 @@ class PLC:
                     struct.unpack("<f", struct.pack("<HH", Raw_RTDs_FP[key].getRegister(1), Raw_RTDs_FP[key].getRegister(0)))[0], 3)
                 # print(key,self.TT_FP_address[key], "RTD",self.TT_FP_dic[key])
 
+            # Set Attributes could be commented(disabled) after it is done
             Attribute_TTFP_address = {}
             Raw_TT_FP_Attribute = {}
             for key in self.TT_FP_address:
