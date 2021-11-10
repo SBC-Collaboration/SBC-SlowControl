02ea2baa950337697c229fed66302c3d9fdb96e3
RunzZhang <runzezhang@ucsb.edu>
2021-11-10 15:15:46 -0800

    heater

--- SlowDAQ_SBC_v2.py
+++ SlowDAQ_SBC_v2.py
@@ -4635,7 +4635,7 @@ class HeaterExpand(QtWidgets.QWidget):
         self.Label.setStyleSheet(TITLE_STYLE + BORDER_STYLE)
         self.Label.setAlignment(QtCore.Qt.AlignCenter)
         self.Label.setText("Label")
-        self.GL.addWidget(self.Label,0,0,0,2)
+        self.GL.addWidget(self.Label,0,0)
 
         self.Mode = DoubleButton(self)
         self.Mode.Label.setText("Mode")
