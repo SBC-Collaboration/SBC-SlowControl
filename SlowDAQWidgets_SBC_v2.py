72f2ef6cbb8d59b05cc70984d232c7bc93c60c29
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2021-11-10 15:59:36 -0800

    Update SlowDAQWidgets_SBC_v2.py

--- SlowDAQWidgets_SBC_v2.py
+++ SlowDAQWidgets_SBC_v2.py
@@ -452,8 +452,8 @@ class HeaterExpand(QtWidgets.QWidget):
 
         self.Label = QtWidgets.QLabel(self)
         self.Label.setObjectName("Label")
-        self.Label.setMinimumSize(QtCore.QSize(30*R, 30*R))
-        # self.Label.setStyleSheet(TITLE_STYLE + BORDER_STYLE)
+        self.Label.setMinimumSize(QtCore.QSize(10*R, 10*R))
+        self.Label.setStyleSheet(TITLE_STYLE + BORDER_STYLE)
         # self.Label.setAlignment(QtCore.Qt.AlignCenter)
         self.Label.setText("Label")
         self.GL.addWidget(self.Label,0,1)
@@ -506,7 +506,6 @@ class HeaterExpand(QtWidgets.QWidget):
         self.FBSwitch = Menu(self)
         self.FBSwitch.Label.setText("FBSWITCH")
         self.GL.addWidget(self.FBSwitch,0,4)
-
 
         self.LOID = Indicator(self)
         self.LOID.Label.setText('LOW')
