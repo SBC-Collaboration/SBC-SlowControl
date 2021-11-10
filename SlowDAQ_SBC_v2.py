9fd8241890e774d9007fd00861419a9619394a16
RunzZhang <runzezhang@ucsb.edu>
2021-11-09 16:37:36 -0800

    heater

--- SlowDAQ_SBC_v2.py
+++ SlowDAQ_SBC_v2.py
@@ -4638,7 +4638,7 @@ class HeaterExpand(QtWidgets.QWidget):
         self.Label.setStyleSheet(TITLE_STYLE + BORDER_STYLE)
         self.Label.setAlignment(QtCore.Qt.AlignCenter)
         self.Label.setText("Label")
-        self.GL.addWidget(self.Label,0,0)
+        self.GL.addWidget(self.Label,0,0,1,0)
 
         # self.HL = QtWidgets.QHBoxLayout()
         # self.HL.setContentsMargins(0*R, 0*R, 0*R, 0*R)
