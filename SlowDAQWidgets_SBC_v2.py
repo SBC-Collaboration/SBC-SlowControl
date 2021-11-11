1b64a73614a37fd77e62718e1b9c98c4377eba53
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2021-11-10 16:00:33 -0800

    Update SlowDAQWidgets_SBC_v2.py

--- SlowDAQWidgets_SBC_v2.py
+++ SlowDAQWidgets_SBC_v2.py
@@ -456,7 +456,7 @@ class HeaterExpand(QtWidgets.QWidget):
         self.Label.setStyleSheet(TITLE_STYLE + BORDER_STYLE)
         # self.Label.setAlignment(QtCore.Qt.AlignCenter)
         self.Label.setText("Label")
-        self.GL.addWidget(self.Label,0,1)
+        self.GL.addWidget(self.Label,0,0)
 
         self.SP = SetPoint(self)
         self.SP.Label.setText("SetPoint")
@@ -501,27 +501,27 @@ class HeaterExpand(QtWidgets.QWidget):
 
         self.Mode = DoubleButton(self)
         self.Mode.Label.setText("Mode")
-        self.GL.addWidget(self.Mode,0,3)
+        self.GL.addWidget(self.Mode,2,3)
 
         self.FBSwitch = Menu(self)
         self.FBSwitch.Label.setText("FBSWITCH")
-        self.GL.addWidget(self.FBSwitch,0,4)
+        self.GL.addWidget(self.FBSwitch,2,4)
 
         self.LOID = Indicator(self)
         self.LOID.Label.setText('LOW')
-        self.GL.addWidget(self.LOID, 0, 5)
+        self.GL.addWidget(self.LOID, 2, 5)
 
         self.HIID = Indicator(self)
         self.HIID.Label.setText('HIGH')
-        self.GL.addWidget(self.HIID, 0, 6)
+        self.GL.addWidget(self.HIID, 2, 6)
 
         self.SETSP = Indicator(self)
         self.SETSP.Label.setText("SP")
-        self.GL.addWidget(self.SETSP,0, 7)
+        self.GL.addWidget(self.SETSP,2, 7)
 
         self.updatebutton= QtWidgets.QPushButton(self)
         self.updatebutton.setText("Update")
-        self.GL.addWidget(self.updatebutton,0,8)
+        self.GL.addWidget(self.updatebutton,2,8)
 
 
 
