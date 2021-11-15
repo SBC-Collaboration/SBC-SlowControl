f68b26716db6ae2ecb08d3524931ca356fff267e
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2021-11-15 15:16:24 -0800

    Update SlowDAQ_SBC_v2.py

--- SlowDAQ_SBC_v2.py
+++ SlowDAQ_SBC_v2.py
@@ -580,8 +580,8 @@ class MainWindow(QtWidgets.QMainWindow):
 
         self.MFC1316 = Heater(self.FluidTab)
         self.MFC1316.move(400*R, 800*R)
-        # self.MFC1316.Label.setText("MFC1316")
-        # self.MFC1316.HeaterSubWindow.setWindowTitle("MFC1316")
+        self.MFC1316.Label.setText("MFC1316")
+        self.MFC1316.HeaterSubWindow.setWindowTitle("MFC1316")
         # self.MFC1316SUB = HeaterExpand(self.MFC1316.HeaterSubWindow)
         # self.MFC1316SUB.Label.setText("MFC1316")
         # # self.MFC1316.HeaterSubWindow.VL.addWidget(self.MFC1316SUB)
