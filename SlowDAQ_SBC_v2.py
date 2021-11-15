af36798bddb38929b269d05bae14a69df9a366cb
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2021-11-15 11:19:41 -0800

    Update SlowDAQ_SBC_v2.py

--- SlowDAQ_SBC_v2.py
+++ SlowDAQ_SBC_v2.py
@@ -4558,7 +4558,7 @@ class HeaterExpand(QtWidgets.QWidget):
         self.Label.setStyleSheet(TITLE_STYLE + BORDER_STYLE)
         self.Label.setAlignment(QtCore.Qt.AlignCenter)
         self.Label.setText("Label")
-        self.GL.addWidget(self.Label,0,1,0,4)
+        self.GL.addWidget(self.Label,0,0,0,4)
 
         self.SP = SetPoint(self)
         self.SP.Label.setText("SetPoint")
