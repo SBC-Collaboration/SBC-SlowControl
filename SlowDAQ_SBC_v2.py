cc0e51e61ad658dda1d7c49c06ba72b93281a7e7
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2021-11-15 15:30:42 -0800

    Update SlowDAQ_SBC_v2.py

--- SlowDAQ_SBC_v2.py
+++ SlowDAQ_SBC_v2.py
@@ -578,7 +578,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.PT2121Fluid.Label.setText("PT2121")
         self.PT2121Fluid.SetUnit(" psi")
 
-        self.MFC1316 = Heater(self.HydraulicTab)
+        self.MFC1316 = Heater(self.FluidTab)
         # self.MFC1316 = Heater(self.ChamberTab)
         self.MFC1316.move(400*R, 800*R)
         self.MFC1316.Label.setText("MFC1316")
