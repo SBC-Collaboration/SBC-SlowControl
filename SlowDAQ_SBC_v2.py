866d767102506818af1dd0ddff3307457ec20bcb
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2021-11-16 10:34:33 -0800

    Update SlowDAQ_SBC_v2.py

--- SlowDAQ_SBC_v2.py
+++ SlowDAQ_SBC_v2.py
@@ -27,18 +27,18 @@ VERSION = "v2.1.3"
 
 # LABEL_STYLE = "background-color: rgb(204,204,204); border-radius: 10px; font-family: \"Calibri\"; " \
 #               "font-size: 18px; font-weight: bold;"
-TITLE_STYLE = "background-color: rgb(204,204,204); border-radius: 10px; font-family: \"Calibri\";" \
-              " font-size: 22px; font-weight: bold;"
+# TITLE_STYLE = "background-color: rgb(204,204,204); border-radius: 10px; font-family: \"Calibri\";" \
+#               " font-size: 22px; font-weight: bold;"
 # SMALL_LABEL_STYLE = "background-color: rgb(204,204,204); border-radius: 6px; font-family: \"Calibri\";" \
 #                     " font-size: 10px;" \
 #                     " font-weight: bold;"
-# LABEL_STYLE = "background-color: rgb(204,204,204); border-radius: 6px; font-family: \"Calibri\"; " \
-#               "font-size: 12px; font-weight: bold;"
-# TITLE_STYLE = "background-color: rgb(204,204,204); border-radius: 10px; font-family: \"Calibri\";" \
-#               " font-size: 14px; font-weight: bold;"
+LABEL_STYLE = "background-color: rgb(204,204,204); border-radius: 6px; font-family: \"Calibri\"; " \
+              "font-size: 12px; font-weight: bold;"
+TITLE_STYLE = "background-color: rgb(204,204,204); border-radius: 10px; font-family: \"Calibri\";" \
+              " font-size: 14px; font-weight: bold;"
 
 SMALL_LABEL_STYLE = " "
-LABEL_STYLE = " "
+# LABEL_STYLE = " "
 # TITLE_STYLE = " "
 
 
