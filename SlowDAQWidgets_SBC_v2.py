6d7d6884917a0987bb36dc6cf83c9520fea50b65
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2021-11-16 10:57:16 -0800

    Update SlowDAQWidgets_SBC_v2.py

--- SlowDAQWidgets_SBC_v2.py
+++ SlowDAQWidgets_SBC_v2.py
@@ -31,7 +31,8 @@ TITLE_STYLE = "background-color: rgb(204,204,204); border-radius: 10px; font-fam
 # BORDER_STYLE = "border-style: outset; border-width: 2px; border-radius: 4px;" \
 #                " border-color: black;"
 
-BORDER_STYLE = " "
+BORDER_STYLE = " border-style: outset; " \
+               " border-color: black;"
 
 
 R=0.6 #Resolution rate
