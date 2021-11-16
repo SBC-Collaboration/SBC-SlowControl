8285dcd48631e469f713d9745a213cbfaf323b91
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2021-11-16 10:47:05 -0800

    stylesheet

--- SlowDAQ_SBC_v2.py
+++ SlowDAQ_SBC_v2.py
@@ -35,10 +35,12 @@ VERSION = "v2.1.3"
 SMALL_LABEL_STYLE = "background-color: rgb(204,204,204); border-radius: 6px; font-family: \"Calibri\";" \
                     " font-size: 10px;" \
                     " font-weight: bold;"
-LABEL_STYLE = "background-color: rgb(204,204,204); border-radius: 6px; font-family: \"Calibri\"; " \
-              "font-size: 12px; font-weight: bold;"
+# LABEL_STYLE = "background-color: rgb(204,204,204); border-radius: 6px; font-family: \"Calibri\"; " \
+#               "font-size: 12px; font-weight: bold;"
 TITLE_STYLE = "background-color: rgb(204,204,204); border-radius: 10px; font-family: \"Calibri\";" \
               " font-size: 14px; font-weight: bold;"
+
+LABEL_STYLE = " "
 
 
 
