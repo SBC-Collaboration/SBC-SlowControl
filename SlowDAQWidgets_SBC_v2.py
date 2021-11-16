1f0a69d90b2899a44cb873a1a1c38a1f8176325e
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2021-11-16 10:52:22 -0800

    label style in GUI part:error

--- SlowDAQWidgets_SBC_v2.py
+++ SlowDAQWidgets_SBC_v2.py
@@ -24,15 +24,13 @@ C_BLUE = "background-color: rgb(34,48,171);"
 C_ORANGE = "background-color: rgb(255,132,27);"
 # TITLE_STYLE = "background-color: rgb(204,204,204); border-radius: 10px; font-family: " \
 #               "\"Calibri\"; font-size: 22px; font-weight: bold;"
-#
+
+#this title style is for SBC slowcontrol machine
 TITLE_STYLE = "background-color: rgb(204,204,204); border-radius: 10px; font-family: " \
               "\"Calibri\"; font-size: 14px; font-weight: bold;"
 BORDER_STYLE = "border-style: outset; border-width: 2px; border-radius: 4px;" \
                " border-color: black;"
-#
 
-
-# BORDER_STYLE = " "
 
 R=0.6 #Resolution rate
 
