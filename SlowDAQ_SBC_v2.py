524125a214210f622c72141b5d81b252d7e1c0a7
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2021-11-17 16:09:19 -0800

    delete super(self super)

--- SlowDAQ_SBC_v2.py
+++ SlowDAQ_SBC_v2.py
@@ -4313,7 +4313,7 @@ class AlarmWin(QtWidgets.QMainWindow):
 
 class HeaterSubWindow(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
-        super(HeaterSubWindow, self).__init__(parent)
+        super().__init__(parent)
 
         self.resize(1200*R, 600*R)
         self.setMinimumSize(1200*R, 600*R)
@@ -4322,46 +4322,33 @@ class HeaterSubWindow(QtWidgets.QMainWindow):
         self.Widget = QtWidgets.QWidget()
         self.Widget.setGeometry(QtCore.QRect(0*R, 0*R, 1200*R, 600*R))
 
-        # self.VL = QtWidgets.QVBoxLayout()
-        # # self.VL = QtWidgets.QVBoxLayout(self)
-        # self.VL.setContentsMargins(0*R, 0*R, 0*R, 0*R)
-        # self.VL.setSpacing(3)
-        # self.VL.setAlignment(QtCore.Qt.AlignCenter)
-        # self.Widget.setLayout(self.VL)
-
-        # self.HL = QtWidgets.QHBoxLayout()
-        # # self.HL = QtWidgets.QHBoxLayout(self)
-        # self.HL.setContentsMargins(0*R, 0*R, 0*R, 0*R)
-        # self.HL.setSpacing(3)
-        # self.HL.setAlignment(QtCore.Qt.AlignCenter)
-        # self.VL.addLayout(self.HL)
 
         self.VL = QtWidgets.QVBoxLayout()
         self.VL.setContentsMargins(0 * R, 0 * R, 0 * R, 0 * R)
-        # self.VL.setAlignment(QtCore.Qt.AlignCenter)
+        self.VL.setAlignment(QtCore.Qt.AlignCenter)
         self.VL.setSpacing(5 * R)
         self.Widget.setLayout(self.VL)
 
         self.HL1 = QtWidgets.QHBoxLayout()
         self.HL1.setContentsMargins(0 * R, 0 * R, 0 * R, 0 * R)
-        # self.HL1.setAlignment(QtCore.Qt.AlignCenter)
+        self.HL1.setAlignment(QtCore.Qt.AlignCenter)
         self.HL1.setSpacing(5 * R)
 
         self.HL2 = QtWidgets.QHBoxLayout()
         self.HL2.setContentsMargins(0 * R, 0 * R, 0 * R, 0 * R)
-        # self.HL2.setAlignment(QtCore.Qt.AlignCenter)
+        self.HL2.setAlignment(QtCore.Qt.AlignCenter)
         self.HL2.setSpacing(5 * R)
 
         self.HL3 = QtWidgets.QHBoxLayout()
         self.HL3.setContentsMargins(0 * R, 0 * R, 0 * R, 0 * R)
-        # self.HL3.setAlignment(QtCore.Qt.AlignCenter)
+        self.HL3.setAlignment(QtCore.Qt.AlignCenter)
         self.HL3.setSpacing(5 * R)
 
         self.VL.addLayout(self.HL1)
         self.VL.addLayout(self.HL2)
         self.VL.addLayout(self.HL3)
 
-        self.Label = QtWidgets.QLabel()
+        self.Label = QtWidgets.QLabel(self)
         self.Label.setObjectName("Label")
         self.Label.setMinimumSize(QtCore.QSize(10 * R, 10 * R))
         self.Label.setStyleSheet("QLabel {" + TITLE_STYLE + BORDER_STYLE + "}")
@@ -4370,11 +4357,11 @@ class HeaterSubWindow(QtWidgets.QMainWindow):
         self.Label.setText("Write")
         self.HL1.addWidget(self.Label)
 
-        self.FBSwitch = Menu()
+        self.FBSwitch = Menu(self)
         self.FBSwitch.Label.setText("FBSWITCH")
         self.HL1.addWidget(self.FBSwitch)
 
-        self.Mode = DoubleButton()
+        self.Mode = DoubleButton(self)
         self.Mode.Label.setText("Mode")
         self.HL1.addWidget(self.Mode)
 
