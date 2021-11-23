38e99fe01922a2edaba3e9adcd9bce3a810a8510
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2021-11-23 14:22:46 -0800

    Update SlowDAQ_SBC_v2.py

--- SlowDAQ_SBC_v2.py
+++ SlowDAQ_SBC_v2.py
@@ -60,7 +60,7 @@ BORDER_STYLE = "  "
 
 SMALL_LABEL_STYLE = "background-color: rgb(204,204,204);  " \
                         " font-size: 10px;" \
-                        " font-weight: bold;"
+                        " "
 LABEL_STYLE = "background-color: rgb(204,204,204);  " \
                   "font-size: 12px; "
 TITLE_STYLE = "background-color: rgb(204,204,204);  " \
