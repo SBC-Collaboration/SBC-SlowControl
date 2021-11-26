d855202c3a157285e789256eada712d50d2c4e10
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2021-11-25 17:00:59 -0800

    Update PLC_init.sh

--- PLC_init.sh
+++ PLC_init.sh
@@ -8,4 +8,4 @@ source /home/hep/Downloads/sbc_slowcontrol/SBC-SlowControl/clear_tcp.sh
 python /home/hep/Downloads/sbc_slowcontrol/SBC-SlowControl/PLC.py
 sleep 2
 done
-echo "quit the loop"
+echo "quit the loop" > ~/ECHO.txt
