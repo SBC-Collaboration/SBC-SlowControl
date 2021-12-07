19760a208751dea2e2742ce5641eb54ce7ad80e3
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2021-12-07 15:46:59 -0800

    Update PLC.py

--- PLC.py
+++ PLC.py
@@ -1391,19 +1391,25 @@ class UpdateServer(QtCore.QObject):
                         self.PLC.LOOPPID_SETPOINT( address= message[key]["address"], setpoint = message[key]["value"]["SETPOINT"], mode = 0)
                         # self.PLC.LOOPPID_HI_LIM(address=message[key]["address"], value=message[key]["value"]["HI_LIM"])
                         # self.PLC.LOOPPID_LO_LIM(address=message[key]["address"], value=message[key]["value"]["LO_LIM"])
-                        self.PLC.LOOPPID_HI_LIM(address=message[key]["address"], value=0)
-                        self.PLC.LOOPPID_LO_LIM(address=message[key]["address"], value=0)
+                        self.PLC.LOOPPID_SET_HI_LIM(address=message[key]["address"],
+                                                    value=message[key]["value"]["HI_LIM"])
+                        self.PLC.LOOPPID_SET_LO_LIM(address=message[key]["address"],
+                                                    value=message[key]["value"]["LO_LIM"])
 
                     elif message[key]["operation"] == "SET1":
                         self.PLC.LOOPPID_SET_MODE(address=message[key]["address"], mode=1)
                         self.PLC.LOOPPID_SETPOINT( address= message[key]["address"], setpoint = message[key]["value"]["SETPOINT"], mode = 1)
-                        self.PLC.LOOPPID_HI_LIM(address=message[key]["address"], value=message[key]["value"]["HI_LIM"])
-                        self.PLC.LOOPPID_LO_LIM(address=message[key]["address"], value=message[key]["value"]["LO_LIM"])
+                        self.PLC.LOOPPID_SET_HI_LIM(address=message[key]["address"],
+                                                    value=message[key]["value"]["HI_LIM"])
+                        self.PLC.LOOPPID_SET_LO_LIM(address=message[key]["address"],
+                                                    value=message[key]["value"]["LO_LIM"])
                     elif message[key]["operation"] == "SET2":
                         self.PLC.LOOPPID_SET_MODE(address=message[key]["address"], mode=2)
                         self.PLC.LOOPPID_SETPOINT( address= message[key]["address"], setpoint = message[key]["value"]["SETPOINT"], mode = 2)
-                        self.PLC.LOOPPID_HI_LIM(address=message[key]["address"], value=message[key]["value"]["HI_LIM"])
-                        self.PLC.LOOPPID_LO_LIM(address=message[key]["address"], value=message[key]["value"]["LO_LIM"])
+                        self.PLC.LOOPPID_SET_HI_LIM(address=message[key]["address"],
+                                                    value=message[key]["value"]["HI_LIM"])
+                        self.PLC.LOOPPID_SET_LO_LIM(address=message[key]["address"],
+                                                    value=message[key]["value"]["LO_LIM"])
                     elif message[key]["operation"] == "SET3":
                         self.PLC.LOOPPID_SET_MODE(address=message[key]["address"], mode=3)
                         self.PLC.LOOPPID_SETPOINT( address= message[key]["address"], setpoint = message[key]["value"]["SETPOINT"], mode = 3)
