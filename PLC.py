3895d99116745ad7debb04e72d979018249753a5
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2021-12-09 13:22:00 -0800

    Update PLC.py

--- PLC.py
+++ PLC.py
@@ -985,7 +985,7 @@ class UpdateDataBase(QtCore.QObject):
                 for key in self.PLC.Valve_OUT:
                     # print(key, self.PLC.Valve_OUT[key] != self.Valve_buffer[key])
                     if self.PLC.Valve_OUT[key] != self.Valve_buffer[key]:
-                        self.db.insert_data_into_datastorage(key + '_OUT', self.dt, self.PLC.Valve_buffer[key])
+                        # self.db.insert_data_into_datastorage(key + '_OUT', self.dt, self.PLC.Valve_buffer[key])
                         self.db.insert_data_into_datastorage(key+'_OUT', self.dt, self.PLC.Valve_OUT[key])
                         self.Valve_buffer[key] = self.PLC.Valve_OUT[key]
                         # print(self.PLC.Valve_OUT[key])
@@ -1001,7 +1001,7 @@ class UpdateDataBase(QtCore.QObject):
                 for key in self.PLC.LOOPPID_EN:
                     # print(key, self.PLC.Valve_OUT[key] != self.Valve_buffer[key])
                     if self.PLC.LOOPPID_EN[key] != self.LOOPPID_buffer[key]:
-                        self.db.insert_data_into_datastorage(key + '_EN', self.dt, self.PLC.LOOPPID_buffer[key])
+                        # self.db.insert_data_into_datastorage(key + '_EN', self.dt, self.PLC.LOOPPID_buffer[key])
                         self.db.insert_data_into_datastorage(key+'_EN', self.dt, self.PLC.LOOPPID_EN[key])
                         self.LOOPPID_buffer[key] = self.PLC.LOOPPID_EN[key]
                         # print(self.PLC.Valve_OUT[key])
