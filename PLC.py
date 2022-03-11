0ae0100897c632fafc5383a4c6f4f3625238ee67
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2022-03-11 10:36:50 -0800

    Update PLC.py

--- PLC.py
+++ PLC.py
@@ -564,15 +564,15 @@ class PLC:
 
             ##########################################################################################
             #procedure
-            Raw_Procedure = {}
-            Raw_Procedure_OUT = {}
-            for key in self.Procedure_address:
-                Raw_Procedure[key] = self.Client_BO.read_holding_registers(self.Procedure_address[key]+1, count=1, unit=0x01)
-
-                self.Procedure_running[key] = self.ReadCoil(1, self.Procedure_address[key])
-                self.Procedure_INTLKD[key] = self.ReadCoil(2, self.Procedure_address[key])
-                self.Procedure_EXIT[key] = struct.unpack(">I", struct.pack(">HH", Raw_Procedure[key].getRegister(0 + 1),
-                                                    Raw_Procedure[key].getRegister(0)))[0]
+            # Raw_Procedure = {}
+            # Raw_Procedure_OUT = {}
+            # for key in self.Procedure_address:
+            #     Raw_Procedure[key] = self.Client_BO.read_holding_registers(self.Procedure_address[key]+1, count=1, unit=0x01)
+            #
+            #     self.Procedure_running[key] = self.ReadCoil(1, self.Procedure_address[key])
+            #     self.Procedure_INTLKD[key] = self.ReadCoil(2, self.Procedure_address[key])
+            #     self.Procedure_EXIT[key] = struct.unpack(">I", struct.pack(">HH", Raw_Procedure[key].getRegister(0 + 1),
+            #                                         Raw_Procedure[key].getRegister(0)))[0]
 
 
             #test the writing function
