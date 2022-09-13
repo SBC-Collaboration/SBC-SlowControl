a50c3431483e290096df55a0a2452c8f22537c9a
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2022-09-13 13:58:12 -0700

    Create bkg_clear.py

---
+++ bkg_clear.py
@@ -0,0 +1,19 @@
+import daemon, os, sys, psutil
+PORT_N=5555
+PROCESS_NAME = "TCP"
+pid = None
+
+
+def clear_tcp():
+    for proc in psutil.process_iter():
+        for conns in proc.connections(kind='tcp'):
+            if conns.laddr.port ==PORT_N:
+        # if PROCESS_NAME in proc.name() &:
+                pid = proc.pid
+                p=psutil.Process(pid)
+                p.terminate()
+                print("\n",pid, "KILLED")
+
+
+if __name__ == "__main__":
+    clear_tcp()
