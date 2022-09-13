5c014d8170e78679e6c35188db7a6527ce256e1c
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2022-09-13 16:15:22 -0700

    Update PLC_daemon.py

--- PLC_daemon.py
+++ PLC_daemon.py
@@ -69,7 +69,7 @@ def PLC_loop():
         try:
             # clear_tcp()
             os.system("date | tee /home/hep/Documents/cron-tutorial/output/daemon_test.txt")
-            os.system("echo 'while loop' | tee -a /home/hep/Documents/cron-tutorial/output/daemon_test.txt")
+            os.system("echo 'while loop $(date)' | tee -a /home/hep/Documents/cron-tutorial/output/daemon_test.txt")
             PLC_body()
         except:
             (type, value, traceback) = sys.exc_info()
