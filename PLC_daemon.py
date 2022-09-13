d1b77fa5ae7188a90c3c2b62c2ec52e44601456c
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2022-09-13 12:09:57 -0700

    Update PLC_daemon.py

--- PLC_daemon.py
+++ PLC_daemon.py
@@ -1186,7 +1186,7 @@ class UpdateDataBase(QtCore.QObject):
         self.Running = True
         while self.Running:
             try:
-                self.dt = datetime_in_s()
+                self.dt = datetime_in_1e5micro()
                 self.early_dt = early_datetime()
                 print("Database Updating", self.dt)
 
@@ -1469,8 +1469,8 @@ class UpdateDataBase(QtCore.QObject):
                 #
                 #     print("No new data from PLC")
                 #     pass
-                # self.db.insert_data_into_datastorage(2,self.dt, 2)
-                self.db.update_data_into_datastorage(2, self.dt, 2)
+                self.db.insert_data_into_datastorage(2,self.dt, 2)
+                # self.db.update_data_into_datastorage(2, self.dt, 2)
                 time.sleep(self.base_period*30)
                 # raise Exception("Test breakup")
             except Exception as e:
