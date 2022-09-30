6b16baf27b83b080a5f261a7282b77cea194c32e
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2022-09-30 11:45:10 -0700

    Update PLC_init.sh

--- PLC_init.sh
+++ PLC_init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PATH=/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/home/hep/.local/bin:/home/hep/bin
 source ~/conda_init.sh
-source /home/hep/miniconda3/bin/activate sbcslowcontrol
+source /home/hep/miniforge3/bin/activate sbcslowcontrol
 which python
 while true; do
 #source /home/hep/PycharmProjects/SBC_slowcontrol_test/clear_tcp.sh
