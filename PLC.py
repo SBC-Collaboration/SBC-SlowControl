26c7be61ee34fca67ff54730b6d9e49a257fae5b
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2022-10-09 11:04:34 -0700

    Update PLC.py

--- PLC.py
+++ PLC.py
@@ -2967,7 +2967,7 @@ class Update(QtCore.QObject):
         self.UpPLC.AI_slack_alarm.connect(self.printstr)
 
         self.UpPLC.AI_slack_alarm.connect(self.message_manager.slack_alarm)
-        self.UpDatabase.DB_ERROR_SIG.connect(self.message_manager.slack_alarm)
+        # self.UpDatabase.DB_ERROR_SIG.connect(self.message_manager.slack_alarm)
 
     def connect_signals(self):
         # self.UpPLC.PLC.DATA_UPDATE_SIGNAL.connect(self.UpDatabase.update_value)
