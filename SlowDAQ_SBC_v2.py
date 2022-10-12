1185aa0ca3433206f97489380d08623420e4fc5f
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2022-10-12 11:32:50 -0700

    Update SlowDAQ_SBC_v2.py

--- SlowDAQ_SBC_v2.py
+++ SlowDAQ_SBC_v2.py
@@ -4712,6 +4712,8 @@ class MainWindow(QtWidgets.QMainWindow):
                 self.Valve_buffer["PV4317"] = received_dic_c["data"]["Valve"]["OUT"]["PV4317"]
             elif not received_dic_c["data"]["Valve"]["Busy"]["PV4317"]:
                 print("PV4317", received_dic_c["data"]["Valve"]["OUT"]["PV4317"] != self.Valve_buffer["PV4317"])
+                print("OUT", received_dic_c["data"]["Valve"]["OUT"]["PV4317"])
+                print("Buffer", self.Valve_buffer["PV4317"])
                 if received_dic_c["data"]["Valve"]["OUT"]["PV4317"] != self.Valve_buffer["PV4317"]:
                     if received_dic_c["data"]["Valve"]["OUT"]["PV4317"]:
                         self.PV4317.Set.ButtonLClicked()
