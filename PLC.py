7756d2bd0343801d8b20a00095106a555b55a173
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2022-11-18 13:10:15 -0800

    Update PLC.py

--- PLC.py
+++ PLC.py
@@ -422,7 +422,8 @@ class PLC(QtCore.QObject):
             Raw_RTDs_FP = {}
             for key in self.TT_FP_address:
                 Raw_RTDs_FP[key] = self.Client.read_holding_registers(self.TT_FP_address[key], count=2, unit=0x01)
-                self.TT_FP_dic[key] = round(
+                # also transform C into K
+                self.TT_FP_dic[key] = 273.15 + round(
                     struct.unpack("<f", struct.pack("<HH", Raw_RTDs_FP[key].getRegister(1), Raw_RTDs_FP[key].getRegister(0)))[0], 3)
 
                 # print(key,self.TT_FP_address[key], "RTD",self.TT_FP_dic[key])
