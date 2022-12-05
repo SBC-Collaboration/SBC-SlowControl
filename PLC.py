e83a544fb712658762ef31974fe4ae66681c1d7a
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2022-12-05 10:05:55 -0800

    Update PLC.py

--- PLC.py
+++ PLC.py
@@ -2112,10 +2112,10 @@ class UpdatePLC(QtCore.QObject):
                     self.check_PT_alarm(keyPT)
                 for keyLEFT_REAL in self.PLC.LEFT_REAL_dic:
                     self.check_LEFT_REAL_alarm(keyLEFT_REAL)
-                for keyDin in self.PLC.Din_dic:
-                    self.check_Din_alarm(keyDin)
-                for keyLOOPPID in self.PLC.LOOPPID_OUT:
-                    self.check_LOOPPID_alarm(keyLOOPPID)
+                # for keyDin in self.PLC.Din_dic:
+                #     self.check_Din_alarm(keyDin)
+                # for keyLOOPPID in self.PLC.LOOPPID_OUT:
+                #     self.check_LOOPPID_alarm(keyLOOPPID)
                 self.or_alarm_signal()
                 time.sleep(self.period)
         except KeyboardInterrupt:
