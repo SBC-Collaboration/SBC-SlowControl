23b067a7c5ad86f1e4898c5d69a5a5124afd7655
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2022-12-06 13:20:00 -0800

    Update SlowDAQWidgets_SBC_v2.py

--- SlowDAQWidgets_SBC_v2.py
+++ SlowDAQWidgets_SBC_v2.py
@@ -4914,7 +4914,7 @@ class Loadfile(QtWidgets.QWidget):
             self.default_dict['data']['LOOPPID']['HI_LIM'][key] = self.high_dic[key]
             self.default_dict['Active']['LOOPPID'][key] = self.active_dic[key]
 
-        print(self.default_dict)
+        print(self.default_dict["Active"])
 
 
     # csv file will lose the data type of the record, we need to recover those properties
