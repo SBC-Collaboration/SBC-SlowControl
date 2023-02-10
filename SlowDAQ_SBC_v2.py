d7238d871a7c0404fd023ba5c19093088772420f
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2023-02-10 10:19:06 -0800

    Update SlowDAQ_SBC_v2.py

--- SlowDAQ_SBC_v2.py
+++ SlowDAQ_SBC_v2.py
@@ -4421,7 +4421,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.TS_ADDREM.expandwindow.FF_RD.Field.setText(bin(received_dic_c["data"]["FF"]["TS_ADDREM_FF"]))
         self.TS_ADDREM.expandwindow.TS_SEL_RD.SetValue(received_dic_c["data"]["PARA_I"]["TS_SEL"])
         self.TS_ADDREM.expandwindow.ADDREM_MASS_RD.SetValue(received_dic_c["data"]["PARA_F"]["TS_ADDREM_MASS"])
-        self.TS_ADDREM.expandwindow.MAXTIME.SetValue(received_dic_c["data"]["PARA_T"]["TS_ADDREM_MAXTIME"])
+        self.TS_ADDREM.expandwindow.MAXTIME_RD.SetValue(received_dic_c["data"]["PARA_T"]["TS_ADDREM_MAXTIME"])
         self.TS_ADDREM.expandwindow.N2MASSTX.SetValue(received_dic_c["data"]["LEFT_REAL"]["value"]["TS_ADDREM_N2MASSTX"])
         self.TS_ADDREM.expandwindow.FLOWET.SetValue(received_dic_c["data"]["PARA_T"]["TS_ADDREM_FLOWET"])
         self.TS_ADDREM.expandwindow.TS1_MASS.SetValue(received_dic_c["data"]["LEFT_REAL"]["value"]["TS1_MASS"])
