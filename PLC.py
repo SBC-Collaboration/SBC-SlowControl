a0abc0eaaed6aed5ab35bcf1f74e37648fc8b051
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2023-03-07 12:12:52 -0800

    Update PLC.py

--- PLC.py
+++ PLC.py
@@ -2263,19 +2263,19 @@ class UpdatePLC(QtCore.QObject):
                         self.check_LOOPPID_alarm(keyLOOPPID)
                     self.or_alarm_signal()
                 except:
-                    # self.PLC.PLC_DISCON_SIGNAL.emit()
+                    self.PLC.PLC_DISCON_SIGNAL.emit()
                     (type, value, traceback) = sys.exc_info()
                     exception_hook(type, value, traceback)
 
                 # if there is alarm, update the PICO watchdog and report the alarm
-                try:
-                    if self.PLC.MainAlarm:
-                        self.alarm_db.ssh_alarm(message=self.alarm_stack)
-                        self.AI_slack_alarm.emit(self.alarm_stack)
-                    else:
-                        self.alarm_db.ssh_write()
-                except:
-                    self.AI_slack_alarm.emit("failed to ssh to PICO watchdog")
+                # try:
+                #     if self.PLC.MainAlarm:
+                #         self.alarm_db.ssh_alarm(message=self.alarm_stack)
+                #         self.AI_slack_alarm.emit(self.alarm_stack)
+                #     else:
+                #         self.alarm_db.ssh_write()
+                # except:
+                #     self.AI_slack_alarm.emit("failed to ssh to PICO watchdog")
 
                 time.sleep(self.period)
 
