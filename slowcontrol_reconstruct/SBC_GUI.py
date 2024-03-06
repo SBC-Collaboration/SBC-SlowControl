994ee546741a9fd8cd2e20f9fb0f38ec5af318c4
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2024-03-06 16:11:39 -0600

    Update SBC_GUI.py

--- slowcontrol_reconstruct/SBC_GUI.py
+++ slowcontrol_reconstruct/SBC_GUI.py
@@ -119,7 +119,7 @@ class MainWindow(QtWidgets.QMainWindow):
             self.Path = os.path.dirname(os.path.realpath(__file__))
         else:
             self.Path = os.getcwd()
-        self.ImagePath = os.path.join(self.Path, "images")
+        self.ImagePath = os.path.join(self.Path, "../images")
 
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
 
@@ -10383,7 +10383,7 @@ class UpdateClient(QtCore.QObject):
             self.client_socket.send(self.commands_package)
             self.client_clear_commands.emit()
         else:
-            self.client_socket.send({})
+            self.client_socket.send(json.dumps({}).encode('utf-8'))
         self.readcommand = True
         print("finished sending commands")
 
