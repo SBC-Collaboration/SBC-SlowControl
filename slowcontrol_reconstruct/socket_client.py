248faa38537666b6b44b4779ebbcad7024e920ce
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2024-03-08 13:45:27 -0600

    Update socket_client.py

--- slowcontrol_reconstruct/socket_client.py
+++ slowcontrol_reconstruct/socket_client.py
@@ -10,32 +10,34 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect to the server
 client_socket.connect((HOST, PORT))
-try:
-    # Create a dictionary to send to the server
-    data_to_send = {'key': 'value', 'number': 42}
+while True:
+    try:
+        # Create a dictionary to send to the server
+        data_to_send = {'key': 'value', 'number': 42}
 
-    # Serialize the dictionary into JSON
-    serialized_data = pickle.dumps(data_to_send)
+        # Serialize the dictionary into JSON
+        serialized_data = pickle.dumps(data_to_send)
 
-    # Send the serialized data to the server
-    client_socket.send(serialized_data)
+        # Send the serialized data to the server
+        client_socket.send(serialized_data)
 
-    # Receive data from the server
-    # data_transfer = b''
-    # while True:
-    #     chunk = client_socket.recv(1024)
-    #     if not chunk:
-    #         break
-    #     data_transfer += chunk
-    data_transfer=client_socket.recv(1024)
-    received_data = pickle.loads(data_transfer)
+        # Receive data from the server
+        # data_transfer = b''
+        # while True:
+        #     chunk = client_socket.recv(1024)
+        #     if not chunk:
+        #         break
+        #     data_transfer += chunk
+        data_transfer = client_socket.recv(1024)
+        received_data = pickle.loads(data_transfer)
 
-    # Deserialize the received JSON data into a dictionary
-    print("received data before load",received_data)
-    # received_dict = json.loads(received_data)
-    # print("Received data from server:", received_dict)
+        # Deserialize the received JSON data into a dictionary
+        print("received data before load", received_data)
+        # received_dict = json.loads(received_data)
+        # print("Received data from server:", received_dict)
 
-    # Close the connection with the server
-    # client_socket.close()
-except Exception as e:
-    print("end")
+        # Close the connection with the server
+        # client_socket.close()
+    except Exception as e:
+        print("end")
+
