2ce3f9e030d9cd7ab22b48aa12e289862064e3a9
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2024-04-01 12:23:34 -0500

    Update BKG_init.sh

--- slowcontrol_reconstruct/BKG_init.sh
+++ slowcontrol_reconstruct/BKG_init.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 PATH=/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/home/hep/.local/bin:/home/hep/bin
+
 if [ -n "$(xrandr --listactivemonitors | grep -i connected)" ]; then
     # Launch the application if a display is connected
     source ~/conda_init.sh
     source /home/hep/miniforge3/bin/activate sbcslowcontrol
     which python
-    while true; do
+    #while true; do
     cd /home/hep/PycharmProjects/SBC_slowcontrol_test/slowcontrol_reconstruct
-    python ./SBC_background.py
+    python ./SBC_background.py &
     sleep 2
-    done
-    echo "quit the loop" > ~/ECHO.txt
+    #done
+    echo "Python application started."
+else
+    echo "No display connected. Skipping application launch."
 fi
 
