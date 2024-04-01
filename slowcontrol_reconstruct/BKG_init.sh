dce552611c37b367fa2415049a1b634fff55a8c7
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2024-04-01 14:53:36 -0500

    Update BKG_init.sh

--- slowcontrol_reconstruct/BKG_init.sh
+++ slowcontrol_reconstruct/BKG_init.sh
@@ -2,18 +2,25 @@
 
 PATH=/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/home/hep/.local/bin:/home/hep/bin
 
-if [ -n "$(xrandr --listactivemonitors | grep -i connected)" ]; then
-    # Launch the application if a display is connected
-    source ~/conda_init.sh
-    source /home/hep/miniforge3/bin/activate sbcslowcontrol
-    which python
-    #while true; do
-    cd /home/hep/PycharmProjects/SBC_slowcontrol_test/slowcontrol_reconstruct
-    python ./SBC_background.py &
-    sleep 2
-    #done
-    echo "Python application started." > /home/hep/bkgshlog.txt
+if [ -n "$DISPLAY" ]; then
+    # Check if the display is a physical desktop display
+    if [[ "$DISPLAY" == :0 ]]; then
+        # Launch the application if a display is connected
+        source ~/conda_init.sh
+        source /home/hep/miniforge3/bin/activate sbcslowcontrol
+        which python
+        #while true; do
+        cd /home/hep/PycharmProjects/SBC_slowcontrol_test/slowcontrol_reconstruct
+        python ./SBC_background.py &
+        sleep 2
+        #done
+        echo "Python application started." > /home/hep/bkgshlog.txt
+    else
+        echo "No display connected. Skipping application launch." > /home/hep/bkgshlog.txt
+    fi
 else
-    echo "No display connected. Skipping application launch." > /home/hep/bkgshlog.txt
+    echo "DISPLAY variable is not set. Exiting."
+    exit 1
+
 fi
 
